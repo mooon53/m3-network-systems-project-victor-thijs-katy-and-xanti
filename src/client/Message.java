6783ef5bfa6214bb4141617007bd2440b63e3274
Frauenfelder, T. (Thijs, Student B-CS) <t.frauenfelder@student.utwente.nl>
2022-04-11 12:16:12 +0200

    Thijs

--- src/client/Message.java
+++ src/client/Message.java
@@ -3,23 +3,23 @@ package client;
 import java.nio.ByteBuffer;
 
 public class Message {
-    private MessageType type;
-    private ByteBuffer data;
+	private MessageType type;
+	private ByteBuffer data;
 
-    public Message(MessageType type){
-        this.type = type;
-    }
+	public Message(MessageType type) {
+		this.type = type;
+	}
 
-    public Message(MessageType type, ByteBuffer data){
-        this.type = type;
-        this.data = data;
-    }
+	public Message(MessageType type, ByteBuffer data) {
+		this.type = type;
+		this.data = data;
+	}
 
-    public MessageType getType(){
-        return type;
-    }
+	public MessageType getType() {
+		return type;
+	}
 
-    public ByteBuffer getData(){
-        return data;
-    }
+	public ByteBuffer getData() {
+		return data;
+	}
 }
