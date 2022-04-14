f9cd10684bc944431f475a51aa3e206cbeba009d
Thijs <t.frauenfelder@student.utwente.nl>
2022-04-14 13:17:26 +0200

    Changed client initialization

--- src/MyProtocol.java
+++ src/MyProtocol.java
@@ -1,5 +1,3 @@
-package Disabled;
-
 import client.*;
 
 import java.io.BufferedReader;
@@ -18,7 +16,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class MyProtocol {
 
-<<<<<<< Updated upstream:src/MyProtocol.java
     // The host to connect to. Set this to localhost when using the audio interface tool.
     private static final String SERVER_IP = "netsys.ewi.utwente.nl"; //"127.0.0.1";
     // The port to connect to. 8954 for the simulation server.
@@ -34,7 +31,7 @@ public class MyProtocol {
         BlockingQueue<Message> receivedQueue = new LinkedBlockingQueue<>();
         BlockingQueue<Message> sendingQueue = new LinkedBlockingQueue<>();
 
-        client = new Client(SERVER_IP, SERVER_PORT, frequency, receivedQueue, sendingQueue); // Give the client the Queues to use
+        client = new Client(SERVER_IP, SERVER_PORT, frequency, receivedQueue, sendingQueue, nodeID); // Give the client the Queues to use
 
         new receiveThread(receivedQueue).start(); // Start thread to handle received messages!
 
@@ -122,85 +119,6 @@ public class MyProtocol {
     public BitSet bytesToBitSet(byte[] input) {
         return BitSet.valueOf(input);
     }
-=======
-	// The host to connect to. Set this to localhost when using the audio interface tool.
-	private static final String SERVER_IP = "netsys.ewi.utwente.nl"; //"127.0.0.1";
-	// The port to connect to. 8954 for the simulation server.
-	private static final int SERVER_PORT = 8954;
-	// The frequency to use.
-	private static int frequency = 500 + 9 * 100;//TODO: Set this to your group frequency!
-
-	private final Client client;
-	private int nodeID;
-
-	public MyProtocol(String server_ip, int server_port, int frequency) {
-		BlockingQueue<Message> receivedQueue = new LinkedBlockingQueue<>();
-		BlockingQueue<Message> sendingQueue = new LinkedBlockingQueue<>();
-
-		client = new Client(SERVER_IP, SERVER_PORT, frequency, receivedQueue, sendingQueue, nodeID); // Give the client the Queues to use
-
-		new receiveThread(receivedQueue).start(); // Start thread to handle received messages!
-
-		// handle sending from stdin from this thread.
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String input;
-			while (!br.ready()) {
-				input = br.readLine(); // read input
-				System.out.println(input);
-				byte[] inputBytes = input.getBytes(); // get bytes from input
-				for (byte i : inputBytes) {
-					System.out.println(i);
-				}
-
-				byte[] packet = new byte[32];
-
-				// source node, ack-flag & data-length
-				packet[0] = bitsetToByte(concatBitSet(new BitSet[]{bytesToBitSet(new byte[]{(byte) nodeID})
-								, new BitSet(1) //TODO: ACK-flag algorithm
-								, bytesToBitSet(new byte[]{(byte) inputBytes.length})})
-				);
-
-				// sequence number; TODO: sequence number
-
-				// syn, next-hop, fragmentation-flag & fragmentation-number
-				packet[2] = bitsetToByte(concatBitSet(new BitSet[]{new BitSet(1), //TODO: syn
-								bytesToBitSet(new byte[]{(byte) nodeID}), //TODO: next-hop
-								new BitSet(1), //TODO: fragmentation-flag
-								new BitSet(4)}) //TODO: fragmentation-number
-				);
-
-				// error detection (ignoring the second and third byte)
-				packet[3] = xorCheck(new Message(
-						MessageType.DATA,
-						ByteBuffer.wrap(concatByteArrays(new byte[]{packet[0]},
-								concatByteArrays(new byte[][]{new byte[]{packet[1]}, inputBytes}))));
-
-				// data
-				System.arraycopy(inputBytes, 0, packet, 4, inputBytes.length);
-
-				ByteBuffer toSend = ByteBuffer.allocate(inputBytes.length); // make a new byte buffer with the length of the input string
-				toSend.put(inputBytes, 0, inputBytes.length); // copy the input string into the byte buffer.
-				Message msg;
-				if ((input.length()) > 2) {
-					msg = new Message(MessageType.DATA, toSend);
-				} else {
-					msg = new Message(MessageType.DATA_SHORT, toSend);
-				}
-				sendingQueue.put(msg);
-			}
-		} catch (InterruptedException | IOException e) {
-			System.exit(2);
-		}
-	}
-
-	public static void main(String[] args) {
-		if (args.length > 0) {
-			frequency = Integer.parseInt(args[0]);
-		}
-		new MyProtocol(SERVER_IP, SERVER_PORT, frequency);
-	}
->>>>>>> Stashed changes:src/Disabled/MyProtocol.java
 
     public byte xorCheck(Message message) {
         byte[] bytes = message.getData().array();
@@ -217,31 +135,14 @@ public class MyProtocol {
         }
     }
 
-<<<<<<< Updated upstream:src/MyProtocol.java
     public byte[] concatByteArrays(byte[] array1, byte[] array2) {
         byte[] result = new byte[array1.length + array2.length];
         System.arraycopy(array1, 0, result, 0, array1.length);
         System.arraycopy(array2, 0, result, array1.length, array2.length);
-=======
-	public byte[] concatByteArrays(byte[][] bytes) {
-		int length = 0;
->>>>>>> Stashed changes:src/Disabled/MyProtocol.java
 
-		for (byte[] byteArray : bytes) {
-			length += byteArray.length;
-		}
+        return result;
+    }
 
-		byte[] output = new byte[length];
-		int currentPos = 0;
-		for (int i = 0; i < bytes.length; i++) {
-			System.arraycopy(bytes[i], 0, output, currentPos, bytes[i].length);
-			currentPos += bytes[i].length;
-		}
-
-		return output;
-	}
-
-<<<<<<< Updated upstream:src/MyProtocol.java
     public BitSet concatBitSet(BitSet[] bitsets) {
         BitSet output = new BitSet();
         int index = 0;
@@ -295,8 +196,7 @@ public class MyProtocol {
                             System.out.print((getBit(firstByte, 0) * 2 + getBit(firstByte, 0) * 1) + "]: ");
                             try {
 
-                                int dataLength =
-                                        16 * getBit(secondByte, 5)
+                                int dataLength = 16 * getBit(secondByte, 5)
                                                 + 8 * getBit(secondByte, 6)
                                                 + 4 * getBit(secondByte, 7)
                                                 + 2 * getBit(thirdByte, 0)
@@ -341,102 +241,4 @@ public class MyProtocol {
             }
         }
     }
-=======
-	public BitSet concatBitSet(BitSet[] bitsets) {
-		BitSet output = new BitSet();
-		int index = 0;
-		for (int i = 0; i < bitsets.length; i++) {
-			if (i>0) index+=bitsets[i-1].length();
-			for (int n = 0; n < bitsets[i].length(); i++) {
-				if (bitsets[i].get(n)) output.set(index+n);
-			}
-		}
-		return output;
-	}
-
-	private class receiveThread extends Thread {
-		private final BlockingQueue<Message> receivedQueue;
-
-		public receiveThread(BlockingQueue<Message> receivedQueue) {
-			super();
-			this.receivedQueue = receivedQueue;
-		}
-
-		public void printByteBuffer(ByteBuffer bytes, int bytesLength) {
-			for (int i = 0; i < bytesLength; i++) {
-				System.out.print(bytes.get(i) + " ");
-			}
-			System.out.println();
-		}
-
-		// Handle messages from the server / audio framework
-		public void run() {
-			while (client.isConnected()) {
-				try {
-					Message m = receivedQueue.take();
-					switch (m.getType()) {
-						case BUSY:
-							System.out.println("BUSY");
-							break;
-						case FREE:
-							System.out.println("FREE");
-							break;
-						case DATA:
-							System.out.print("DATA: ");
-							printByteBuffer(m.getData(), m.getData().capacity()); //Just print the data
-							try {
-								for (int i = 0; i < m.getData().capacity(); i++) {
-									System.out.print(Character.toString(m.getData().get(i)));
-									/*BitSet bitset = BitSet.valueOf(m.getData());
-									for (int n = 0; n<bitset.length(); n++) {
-										int bit = bitset.get(n) ? 1:0;
-										System.out.print(bit);
-									}*/
-								}
-							} catch (IllegalArgumentException e) {
-								System.out.println(";");
-							}
-							break;
-						case DATA_SHORT:
-							System.out.print("DATA_SHORT: ");
-							printByteBuffer(m.getData(), m.getData().capacity()); //Just print the data
-							System.out.println(new String(m.getData().array()).trim());
-							try {
-								for (int i = 0; i < m.getData().capacity(); i++) {
-									System.out.print(Character.toString(m.getData().get(i)));
-									/*BitSet bitset = BitSet.valueOf(m.getData());
-									for (int n = 0; n<bitset.length(); n++) {
-										int bit = bitset.get(n) ? 1:0;
-										System.out.print(bit);
-									}*/
-								}
-								System.out.println(";");
-							} catch (IllegalArgumentException e) {
-								System.out.println(";");
-							}
-							break;
-						case DONE_SENDING:
-							System.out.println("DONE_SENDING");
-							break;
-						case HELLO:
-							System.out.println("HELLO");
-							break;
-						case SENDING:
-							System.out.println("SENDING");
-							break;
-						case END:
-							System.out.println("END");
-							System.exit(0);
-							break;
-						default:
-							System.out.println();
-							break;
-					}
-				} catch (InterruptedException e) {
-					System.err.println("Failed to take from queue: " + e);
-				}
-			}
-		}
-	}
->>>>>>> Stashed changes:src/Disabled/MyProtocol.java
 }
